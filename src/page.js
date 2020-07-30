94f7854b94a17cddf80cbee7952d9df5bc8772b9
Ben <ben10baek@hotmail.com>
2020-07-30 13:55:18 -0700

    Added a Page class for dom

--- src/page.js
+++ src/page.js
@@ -1,3 +1,25 @@
 const Page = () => {
-  const body = document.body;
+  const content = document.createElement('div');
+  const main = document.createElement('main');
+  const footer = document.createElement('footer');
+
+  const loadPage = () => {
+    content.appendChild(main);
+    content.appendChild(footer);
+  };
+
+  const setStyle = () => {
+    content.id = 'content';
+    main.id = 'site-main';
+    footer.id = 'site-footer';
+  };
+
+  const start = () => {
+    loadPage();
+    setStyle();
+  };
+
+  return { start };
 };
+
+export { Page };
